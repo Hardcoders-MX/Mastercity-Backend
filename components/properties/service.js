54f4d4f2650a66581c9ec0b3a383b8cd864ffff8
FrcGustavo <hidalgo_fco520@outlook.com>
2020-05-08 16:59:33 -0500

    add service to create a property

--- components/properties/service.js
+++ components/properties/service.js
@@ -1,6 +1,60 @@
 const Property = require('./model');
 const buildParams = require('../../utils/buildParams');
 
+/**
+ * receive parameters and filter with only valid params
+ * @param {Object} params
+ */
+const validateParams = (params) => {
+  const validParams = [
+    'propertyType',
+    'location',
+    'price',
+    'rooms',
+    'bathrooms',
+    'square',
+    'priceMeters',
+    'furnish',
+    'parking',
+    'swimmingPool',
+    'heating',
+    'security',
+    'cellar',
+    'elevator',
+  ];
+  return buildParams(validParams, params);
+};
+
+/**
+ * Validate that required params it existed
+ * @param {Object} params
+ */
+const validateRequiredParams = (params) => {
+  const requiredParams = [
+    'propertyType',
+    'location',
+    'price',
+    'rooms',
+    'bathrooms',
+    'square',
+    'priceMeters',
+    'furnish',
+    'parking',
+    'swimmingPool',
+    'heating',
+    'security',
+    'cellar',
+    'elevator',
+  ];
+  requiredParams.forEach((field) => {
+    if (!params[field]) {
+      throw new Error(`Field ${field}`);
+    }
+  });
+
+  return true;
+};
+
 const findAll = async (filters) => {
   const limit = Number(filters.limit) || 10;
   const sortName = filters.sort_name ? String(filters.sort_name) : '_id';
@@ -45,6 +99,27 @@ const findAll = async (filters) => {
   return { properties, pagination };
 };
 
+/**
+ * Inert one property in the database
+ * @param {Property} property
+ */
+const insert = async (property) => {
+  const params = validateParams(property);
+  const isAprove = false;
+  const isDisable = false;
+
+  validateRequiredParams(params);
+
+  const createdProperty = await Property.create({
+    ...params,
+    isAprove,
+    isDisable,
+  });
+
+  return createdProperty;
+};
+
 module.exports = {
   findAll,
+  insert,
 };
