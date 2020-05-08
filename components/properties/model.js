9d6752b4a0310ba08103e05a52d809035afb940a
FrcGustavo <hidalgo_fco520@outlook.com>
2020-05-07 23:42:26 -0500

    Add field to model and add one util to valid parameters

--- components/properties/model.js
+++ components/properties/model.js
@@ -2,7 +2,72 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const propertySchema = new Schema({}, {
+const propertySchema = new Schema({
+  propertyType: {
+    type: String,
+    enum: ['house', 'department', 'office', 'studio'],
+  },
+  location: {
+    lat: Number,
+    len: Number,
+  },
+  price: {
+    type: Number,
+    require: true,
+  },
+  rooms: {
+    type: Number,
+    require: true,
+  },
+  bathrooms: {
+    type: Number,
+    require: true,
+  },
+  square: {
+    type: Number,
+    require: true,
+  },
+  priceMeters: {
+    type: Number,
+    required: true,
+  },
+  furnish: {
+    type: Boolean,
+    require: true,
+  },
+  parking: {
+    type: Boolean,
+    require: true,
+  },
+  swimmingPool: {
+    type: Boolean,
+    required: true,
+  },
+  heating: {
+    type: Boolean,
+    required: true,
+  },
+  security: {
+    type: Boolean,
+    required: true,
+  },
+  cellar: {
+    type: Boolean,
+    required: true,
+  },
+  elevator: {
+    type: Boolean,
+    required: true,
+  },
+  isAprove: {
+    type: Boolean,
+    required: true,
+  },
+  isDisable: {
+    type: Boolean,
+    required: true,
+  },
+}, {
   timestamps: true,
 });
 
