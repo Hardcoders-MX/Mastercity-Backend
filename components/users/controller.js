8620ed8ca879a82e61e3bb98968cd0c797ceddc9
Solej77 <ing.josgalo@gmail.com>
2020-05-10 23:13:35 -0500

    Partially removed of one applicant

--- components/users/controller.js
+++ components/users/controller.js
@@ -1,3 +1,4 @@
+const config = require('../../config');
 const serviceUser = require('./service');
 const { success } = require('../../routes/response');
 
@@ -17,6 +18,20 @@ const getUser = async (req, res, next) => {
   }
 };
 
+const deleteUser = async (req, res, next) => {
+  const userId = req.params.id;
+
+  try {
+    let data = '';
+    const user = await serviceUser.disable(userId);
+    config.srv.mode === 'development' ? data = user : data = 'OK';
+    success(res, 'User deleted', data, 200);
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
-  getUser
+  getUser,
+  deleteUser,
 };
--- components/users/routes.js
+++ components/users/routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
-
 const router = express.Router();
 
 const {
   getUser,
+  deleteUser,
+
 } = require('./controller');
 
 router.get('/:id', getUser);
+router.patch('/:id', deleteUser);
 
 module.exports = router;
