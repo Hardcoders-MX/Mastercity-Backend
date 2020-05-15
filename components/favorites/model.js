90cf91c1bfd8e97397367720da5912aeefc1ae59
FrcGustavo <hidalgo_fco520@outlook.com>
2020-05-15 17:20:34 -0500

    [FEATURE] add model and protected routes of favorites

---
+++ components/favorites/model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const favoriteSchema = new Schema({
+  user: {
+    tpye: Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  property: {
+    type: Schema.Types.ObjectId,
+    ref: 'properties',
+  },
+}, {
+  timestamps: true,
+});
+
+const Favorite = mongoose.model('favorites', favoriteSchema);
+
+module.exports = Favorite;
--- components/favorites/routes.js
+++ components/favorites/routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const passport = require('passport');
+
 const controller = require('./controller');
 
 const router = express.Router();
 
-router.get('/:user', controller.index);
-router.post('/', controller.create);
+require('../auth/strategies/jwt');
+
+router.get('/:user', passport.authenticate('jwt', { session: false }), controller.index);
+router.post('/', passport.authenticate('jwt', { session: false }), controller.create);
 
 module.exports = router;
