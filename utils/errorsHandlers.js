251abd1f4c3328f4f6ce29fcb6ed95233c04860b
FrcGustavo <hidalgo_fco520@outlook.com>
2020-05-26 15:04:56 -0500

    📝[Documenting] properties

--- utils/errorsHandlers.js
+++ utils/errorsHandlers.js
@@ -6,11 +6,12 @@ const {
   sentryDns,
   sentryId,
 } = config.sentry;
-
-Sentry.init({ dsn: `https://${sentryDns}@o361676.ingest.sentry.io/${sentryId}` });
+if (config.srv.mode === 'production') {
+  Sentry.init({ dsn: `https://${sentryDns}@o361676.ingest.sentry.io/${sentryId}` });
+}
 
 function logErrors(err, req, res, next) {
-  Sentry.captureException(err);
+  if (config.srv.mode === 'production') Sentry.captureException(err);
   debug(err.stack);
   next(err);
 }
